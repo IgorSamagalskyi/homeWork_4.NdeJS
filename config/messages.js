91367b62116c14234773cde73618ea955e9aa23f
IgorSamagalskyi <batelgeizea@gmail.com>
2021-08-27 18:13:36 +0300

    fix remark and add status codes Variables.

--- config/messages.js
+++ config/messages.js
@@ -1,5 +1,5 @@
 module.exports = {
-    CAR_NOT_FOUND: 'Car not found',
+    CAR_NOT_FOUND: 'Car not found ',
     USER_NOT_FOUND: 'User not found',
     EMPTY_FIELDS: 'All fields must be filled in!',
     EMAIL_EXIST: 'This email is already in use, please select another email.',
